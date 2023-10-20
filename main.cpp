ec07feaa1d6b509c73b0d136f925ea88a02da231
Eric <ericcom@outlook.com>
2023-10-20 15:09:12 -0400

    Fuck (but in a good way)

--- main.cpp
+++ main.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 int part_1(const vector<string> data);
 int part_2(const vector<string> data);
-string reverse_capitalization(string line);
+
+// this means i need to substract by 48 to get numerical value of character
+string normalize_to_49(string line);
 string char_to_string(char *a);
 
 int main(int argc, char const *argv[])
@@ -16,8 +18,8 @@ int main(int argc, char const *argv[])
     /* code */
     vector<string> data = read_input("./input");
 
-    cout << data[0] << endl;
-    cout << endl;
+    string a =normalize_to_49("a");
+    cout << (int)a[0] << endl;
 
     // cout << part_1(data) << endl;
     // cout << part_2(data) << endl;
@@ -25,31 +27,19 @@ int main(int argc, char const *argv[])
     return 0;
 }
 
-string reverse_capitalization(string line)
+string normalize_to_49(string line)
 {
-    char newLine[line.length() + 1];
+    string newLine;
 
     for (int i = 0; i < line.length(); i++)
     {
-        if ((int)line[i] >= 65 || (int)line[i] <= 90)
-        {
-            newLine[i] += line[i] + 32;
-        }
+        if ((int)line[i] >= 65 && (int)line[i] <= 90)
+            newLine += line[i] + 10;
         else
-        {
-            newLine[i] -= line[i] - 32;
-        }
+            newLine += line[i] - 48;
     }
 
-    newLine[sizeof(newLine) / sizeof(char) - 1] = '\0';
-
-    return char_to_string(newLine);
-}
-
-string char_to_string(char *a)
-{
-    string s = a;
-    return s;
+    return newLine;
 }
 
 int part_1(const vector<string> data)
