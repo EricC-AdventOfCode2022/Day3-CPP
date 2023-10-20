c8fd77be47411cd78b8d2e873df05140fb2ca1fa
Eric <ericcom@outlook.com>
2023-10-20 17:48:47 -0400

    Shit isn't borked if you count manually (but it's still borked)

--- main.cpp
+++ main.cpp
@@ -9,44 +9,94 @@ using namespace std;
 int part_1(const vector<string> data);
 int part_2(const vector<string> data);
 
+vector<string> copy_data(const vector<string> data);
+
 // this means i need to substract by 48 to get numerical value of character
-string normalize_to_49(string line);
+vector<string> normalize_to_49(vector<string> data);
 string char_to_string(char *a);
 
 int main(int argc, char const *argv[])
 {
     /* code */
     vector<string> data = read_input("./input");
+    vector<string> normalizedData = normalize_to_49(copy_data(data));
 
-    string a =normalize_to_49("a");
-    cout << (int)a[0] << endl;
-
-    // cout << part_1(data) << endl;
-    // cout << part_2(data) << endl;
+    // 13250 too high
+    // 9954 too high
+    // 984 too low
+    // 7446 correct (counted manually, find way to get it to show)
+    cout << part_1(normalizedData) << endl;
+    cout << part_2(normalizedData) << endl;
 
     return 0;
 }
 
-string normalize_to_49(string line)
+vector<string> normalize_to_49(vector<string> data)
 {
-    string newLine;
+    vector<string> normalized;
 
-    for (int i = 0; i < line.length(); i++)
+    for (int i = 0; i < data.size(); i++)
     {
-        if ((int)line[i] >= 65 && (int)line[i] <= 90)
-            newLine += line[i] + 10;
-        else
-            newLine += line[i] - 48;
+        string line = data[i];
+        string newLine = "";
+
+        for (int j = 0; j < line.length(); j++)
+        {
+            if ((int)line[j] >= 65 && (int)line[j] <= 90)
+                newLine += (char)((int)line[j] + 10);
+            else
+                newLine += (char)((int)line[j] - 48);
+        }
+        normalized.push_back(newLine);
     }
 
-    return newLine;
+    return normalized;
+}
+vector<string> copy_data(const vector<string> data)
+{
+    vector<string> copy;
+
+    for (int i = 0; i < data.size(); i++)
+    {
+        copy.push_back(data[i]);
+    }
+
+    return copy;
 }
 
 int part_1(const vector<string> data)
 {
+    int score = 0;
+    vector<char> characters;
 
+    for (int i = 0; i < data.size(); i++)
+    {
+        string half1 = "";
+        string half2 = "";
+
+        for (int j = 0; j < data[i].length(); j++)
+        {
+            if (j > data[i].length() / 2 - 1)
+                half1 += data[i][j];
+            else
+                half2 += data[i][j];
+        }
+        for (int j = 0; j < half1.length(); j++)
+        {
+            for (int k = 0; k < half2.length(); k++)
+            {
+                if(half1[j] == half2[k])
+                    characters.push_back(half1[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < characters.size(); i++)
+    {
+        cout << characters[i] << endl;
+    }
+    return score;
 }
 int part_2(const vector<string> data)
 {
-
 }
